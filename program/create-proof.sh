ad8a5f5fd15f3b20e90a20b4a2ff69b830f802c5
gskapka <gregkapka@gmail.com>
2024-03-12 16:21:55 +0000

    chore(initial-commit): <- adds that

---
+++ program/create-proof.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+CC_riscv32im_succinct_zkvm_elf=~/riscv/bin/riscv32-unknown-elf-gcc cargo prove build
---
+++ script/src/main.rs
@@ -0,0 +1,34 @@
+//! A simple script to generate and verify the proof of a given program.
+
+use std::fs::read_to_string;
+use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier, utils};
+
+const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
+
+fn main() {
+    //utils::setup_tracer();
+
+    let mut args = std::env::args();
+    let path = args.nth(1).expect("please supply an argument");
+    let s = read_to_string(&path).expect(&format!("could not read file at path: '{path}'"));
+
+    // Generate proof.
+    let mut stdin = SP1Stdin::new();
+    stdin.write(&s);
+
+    let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
+
+    // Read output.
+    let r = proof.stdout.read::<bool>();
+    println!("proof result r: {r}");
+
+    // Verify proof.
+    SP1Verifier::verify(ELF, &proof).expect("verification failed");
+
+    // Save proof.
+    proof
+        .save("proof-with-io.json")
+        .expect("saving proof failed");
+
+    println!("succesfully generated and verified proof for the program!")
+}
