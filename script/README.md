0f0792d146ac583fafb364b0f10edf0d11a96184
gskapka <gregkapka@gmail.com>
2024-03-15 11:24:29 +0000

    feat(script): <- adds error checking for elf existence

--- script/README.md
+++ script/README.md
@@ -1,3 +1,5 @@
 Run via:
 
 `RUST_LOG=<info|debug|warn> cargo run --release`
+
+To build the elf required to run this, please see: `../program/README.md`
--- script/src/main.rs
+++ script/src/main.rs
@@ -1,20 +1,24 @@
 //! A simple script to generate and verify the proof of a given program.
 
 use sp1_core::{utils, SP1Prover, SP1Stdin, SP1Verifier};
-use std::fs::read_to_string;
+use std::{
+    fs::{read, read_to_string},
+    path::Path,
+};
 
-const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
+const ELF_PATH: &str = "../../program/elf/riscv32im-succinct-zkvm-elf";
 
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(version, about, long_about = None, rename_all="camelCase")]
 struct Cli {
     #[command(subcommand)]
     commands: Commands,
 }
 
 #[derive(Subcommand)]
+#[command(rename_all = "camelCase")]
 enum Commands {
     /// Generate proof
     GenerateProof {
@@ -36,20 +40,29 @@ fn main() {
             let s = read_to_string(&path)
                 .unwrap_or_else(|_| panic!("could not read file at path: {path}"));
 
-            // Generate proof.
+            // NOTE: Check the elf exists
+            if !Path::new(ELF_PATH).exists() {
+                panic!(
+                    "elf does not exist at path: {ELF_PATH}, see the readme for how to create it!"
+                );
+            };
+
+            let elf_bytes = read(ELF_PATH).expect("this to work because of above check");
+
+            // NOTE:  Generate proof.
             let mut stdin = SP1Stdin::new();
             stdin.write(&s);
 
-            let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
+            let mut proof = SP1Prover::prove(elf_bytes.as_slice(), stdin).expect("proving failed");
 
-            // Read output.
+            // NOTE: Read output.
             let r = proof.stdout.read::<bool>();
             println!("proof result r: {r}");
 
-            // Verify proof.
-            SP1Verifier::verify(ELF, &proof).expect("verification failed");
+            // NOTE: Verify proof.
+            SP1Verifier::verify(elf_bytes.as_slice(), &proof).expect("verification failed");
 
-            // Save proof.
+            // NOTE: Save proof.
             proof
                 .save("proof-with-io.json")
                 .expect("saving proof failed");
