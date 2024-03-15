b4f0906aa8318d969b318893db8daff380e7f154
gskapka <gregkapka@gmail.com>
2024-03-15 10:13:51 +0000

    feat(script): <- adds tracing to that

--- script/src/main.rs
+++ script/src/main.rs
@@ -1,12 +1,12 @@
 //! A simple script to generate and verify the proof of a given program.
 
 use std::fs::read_to_string;
-use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier, utils};
+use sp1_core::{utils, SP1Prover, SP1Stdin, SP1Verifier};
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
-    //utils::setup_tracer();
+    utils::setup_tracer();
 
     let mut args = std::env::args();
     let path = args.nth(1).expect("please supply an argument");
