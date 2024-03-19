76cf3ee5927a11de3b5eed2e75ea2651706c1364
gskapka <gregkapka@gmail.com>
2024-03-19 20:15:48 +0000

    feat(bitcoin): <- adds optional path to write files to

--- bitcoin/src/write_blocks.rs
+++ bitcoin/src/write_blocks.rs
@@ -2,12 +2,18 @@ use crate::{btc_blocks::BtcBlocks, btc_blocks_wrapper::BtcBlocksWrapper, BtcErro
 use std::fs::File;
 use std::io::prelude::*;
 
-const BLOCKS_PATH: &str = "blocks.json";
+const DEFAULT_BLOCKS_PATH: &str = "blocks.json";
 
-pub fn write_blocks_to_file(blocks: BtcBlocks) -> Result<(), BtcError> {
-    let mut file = File::create(BLOCKS_PATH)?;
+pub fn write_blocks_to_file(blocks: BtcBlocks, maybe_path: Option<String>) -> Result<(), BtcError> {
+    let mut file = if let Some(s) = maybe_path {
+        debug!("writing output to {s}");
+        File::create(s)
+    } else {
+        debug!("writing output to {DEFAULT_BLOCKS_PATH}");
+        File::create(DEFAULT_BLOCKS_PATH)
+    }?;
     let bytes: Vec<u8> = BtcBlocksWrapper::new(blocks.iter().cloned().collect()).try_into()?;
     file.write_all(&bytes)?;
-    info!("blocks written to path: {BLOCKS_PATH}");
+    debug!("blocks written successfully");
     Ok(())
 }
