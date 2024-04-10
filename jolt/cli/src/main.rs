06a0328dc55394cc2fda939e719ae0e2692d7b33
Matteo Lisotto <matteo.lisotto@gmail.com>
2024-04-10 15:32:49 +0200

    feat(jolt): add guest and cli

--- jolt/cli/src/main.rs
+++ jolt/cli/src/main.rs
@@ -1,9 +1,51 @@
-pub fn main() {
-    let (prove_fib, verify_fib) = guest::build_fib();
+use std::fs::read_to_string;
 
-    let (output, proof) = prove_fib(50);
-    let is_valid = verify_fib(proof);
+use bitcoin::{get_blocks, write_blocks_to_file, BtcError, MAX_NUM_BLOCKS};
+use clap::Parser;
 
-    println!("output: {}", output);
-    println!("valid: {}", is_valid);
+use cli::{Cli, Commands};
+
+async fn handle_cli(cli: Cli) -> Result<(), BtcError> {
+    match cli.commands() {
+        Commands::GetBlocks {
+            start,
+            amount,
+            output,
+            rpc_endpoint,
+        } => {
+            if *amount > MAX_NUM_BLOCKS {
+                return Err(BtcError::TooManyBlocks(*amount));
+            };
+            let blocks = get_blocks(rpc_endpoint, *start, *amount).await?;
+            write_blocks_to_file(blocks, output.clone())?;
+
+            Ok(())
+        }
+
+        Commands::GenerateProof { blocks_path, .. } => {
+            let s = read_to_string(blocks_path)
+                .unwrap_or_else(|_| panic!("could not read file at path: {blocks_path}"));
+
+            // NOTE: Retrieve prover and verifier.
+            let (prove_blocks, verify_blocks) = guest::build_check_blocks();
+
+            // NOTE: Produce a receipt.
+            let (_output, proof) = prove_blocks(&s);
+
+            // NOTE: Verify proof.
+            if verify_blocks(proof) {
+                println!("successfully generated and verified proof for the blocks!");
+            } else {
+                println!("error on verifying proof for the blocks!");
+            }
+
+            Ok(())
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+    handle_cli(cli).await.unwrap(); // FIXME
 }
--- jolt/guest/src/lib.rs
+++ jolt/guest/src/lib.rs
@@ -1,16 +1,14 @@
 #![cfg_attr(feature = "guest", no_std)]
 #![no_main]
 
-#[jolt::provable]
-fn fib(n: u32) -> u128 {
-    let mut a: u128 = 0;
-    let mut b: u128 = 1;
-    let mut sum: u128;
-    for _ in 1..n {
-        sum = a + b;
-        a = b;
-        b = sum;
-    }
+use std::str::FromStr;
 
-    b
+use bitcoin::BtcBlocks;
+use lc::prove_btc_blocks;
+
+#[jolt::provable]
+fn check_blocks(blocks: &str) -> bool {
+    let blocks = BtcBlocks::from_str(blocks).expect("to unwrap btc blocks");
+
+    prove_btc_blocks(blocks)
 }
